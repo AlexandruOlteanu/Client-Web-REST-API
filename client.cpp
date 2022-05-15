7159baca2f63cffa5eb5855dbb5da6db9fac51ca
Alexandru <alexandruolteanu2001@gmail.com>
2022-05-15 06:16:58 -0700

    Starting

---
+++ client.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stdio.h>      /* printf, sprintf */
+#include <stdlib.h>     /* exit, atoi, malloc, free */
+#include <unistd.h>     /* read, write, close */
+#include <string.h>     /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h>      /* struct hostent, gethostbyname */
+#include <arpa/inet.h>
+#include "helpers.h"
+#include "requests.h"
+#include "error.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ERROR(argc != 1, "Error, number of arguments");
+
+    string message;
+    string response;
+    int32_t server_socket = -1;
+    
+       
+    
+
+    return 0;
+}
--- requests.cpp
+++ requests.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 #include <stdlib.h>     /* exit, atoi, malloc, free */
 #include <stdio.h>
 #include <unistd.h>     /* read, write, close */
@@ -9,11 +10,13 @@
 #include "helpers.h"
 #include "requests.h"
 
+using namespace std;
+
 char *compute_get_request(char *host, char *url, char *query_params,
                             char **cookies, int cookies_count)
 {
-    char *message = calloc(BUFLEN, sizeof(char));
-    char *line = calloc(LINELEN, sizeof(char));
+    char *message = (char *) calloc(BUFLEN, sizeof(char));
+    char *line = (char *) calloc(LINELEN, sizeof(char));
 
     // Step 1: write the method name, URL, request params (if any) and protocol type
     if (query_params != NULL) {
@@ -25,9 +28,16 @@ char *compute_get_request(char *host, char *url, char *query_params,
     compute_message(message, line);
 
     // Step 2: add the host
+    sprintf(line, "HostL %s", host);
+    compute_message(message, line);
     // Step 3 (optional): add headers and/or cookies, according to the protocol format
-    if (cookies != NULL) {
-       
+    if (cookies != NULL && cookies_count > 0) {
+       sprintf(line, "Cookie: ");
+       for (int i = 0; i < cookies_count; ++i) {
+           sprintf(line + strlen(line), " %s;", cookies[i]);
+       }
+       line[strlen(line) - 1] = '\n';
+       compute_message(message, line);
     }
     // Step 4: add final new line
     compute_message(message, "");
@@ -37,24 +47,44 @@ char *compute_get_request(char *host, char *url, char *query_params,
 char *compute_post_request(char *host, char *url, char* content_type, char **body_data,
                             int body_data_fields_count, char **cookies, int cookies_count)
 {
-    char *message = calloc(BUFLEN, sizeof(char));
-    char *line = calloc(LINELEN, sizeof(char));
-    char *body_data_buffer = calloc(LINELEN, sizeof(char));
+    char *message = (char *) calloc(BUFLEN, sizeof(char));
+    char *line = (char *) calloc(LINELEN, sizeof(char));
+    char *body_data_buffer = (char *) calloc(LINELEN, sizeof(char));
+
+    for (int i = 0; i < body_data_fields_count; ++i) {
+        strcat(body_data_buffer, body_data[i]);
+        if (i < body_data_fields_count - 1) {
+            strcat(body_data_buffer, "&");
+        }
+    }
 
     // Step 1: write the method name, URL and protocol type
-    sprintf(line, "POST %s HTTP/1.1", url);
+    sprintf(line, "POST: %s HTTP/1.1", url);
     compute_message(message, line);
     
     // Step 2: add the host
+    sprintf(line, "Host: %s", host);
+    compute_message(message, line);
     /* Step 3: add necessary headers (Content-Type and Content-Length are mandatory)
             in order to write Content-Length you must first compute the message size
     */
+    sprintf(line, "Content-Type: %s", content_type);
+    compute_message(message, line);
+
+    sprintf(line, "Content-Length %d", (int) strlen(body_data_buffer));
+    compute_message(message, line);
     // Step 4 (optional): add cookies
-    if (cookies != NULL) {
-       
+    if (cookies != NULL && cookies_count > 0) {
+       sprintf(line, "Cookie: ");
+       compute_message(message, line);
+       for (int i = 0; i < cookies_count; ++i) {
+           sprintf(line + strlen(line), " %s;", cookies[i]);
+       }
+       line[strlen(line) - 1] = '\0';
+       compute_message(message, line);
     }
     // Step 5: add new line at end of header
-
+    compute_message(message, "");
     // Step 6: add the actual payload data
     memset(line, 0, LINELEN);
     compute_message(message, body_data_buffer);
