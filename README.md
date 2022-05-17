65e367aa8827335ca38dd9bdcc4108b7380231b4
Alexandru <alexandruolteanu2001@gmail.com>
2022-05-17 08:23:52 -0700

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Client-Web---REST-API
+<Copyright Alexandru OLteanu, grupa 322CA, alexandruolteanu2001@gmail.com>
+
+    Tema mi s-a parut foarte interesanta, ideea de simulare a unei asa zise 
+platforme folosind request-uri HTTP de tip GET, POST si DELETE a fost destul 
+de usor de implementat dar pot spune ca ne-a familiarizat destul de bine 
+cu conceptul. Am ales sa rezolv cerintele in C++, alegere facuta din motivul
+lucrului mai usor cu sirurile de caractere comparativ cu C. Am folosit
+biblioteca json recomandata in documentatie, a fost destul de usor de inteles
+cum se foloseste si overall a fost foarte ok.
+    Arhiva cuprinde fisierele helpers.cpp, helpers.h, buffer.cpp si buffer.h 
+preluate din laborator ce realizeaza trimiterea unui request odata construit catre 
+server si primirea rezultatului din partea acestuia. Aceste fisiere nu au fost 
+modificate de catre mine. Urmatoarele fisiere sunt cele de request care erau putin 
+mai complicate si incomplete, astfel am modificat functiile folosind string-uri, codul
+fiind acum mult mai lizibil si usor de inteles. Am adaugat de asemenea partea de 
+Autorizare pentru token-ul Jwt si am creat functia pentru DELETE. O ultima modificare
+adusa este eliminarea pointer-ului dublu pentru cookie si token, deoarece in solutia
+mea am trimis direct mesajul construit sub forma unui obiect formatat json.
+    In cadrul clientului am implementat functiile necesare pentru exit, register, login,
+enter_library, get_books, get_book, add_book, delete_book si logout. Detalierea pentru
+aceste functii se gaseste in cod, insa in mare parte acestea au fost asemanatoare. In
+prima faza trebuia deschisa conexiunea catre server, apoi verificate ca si comenzi valide
+fiecare dintre ele. Fiecare functie a urmat ideea de construire a mesajului trimis sub forma 
+unui string, unele ofereau promt pentru interactiunea cu userul. In final, dupa trimiterea
+request-ului catre server, raspunsul era primit, acesta fiind tratat in functie de erori si
+de succes.
+    Solutia a fost conceputa sa accepte usor modificari viitoare, astfel am construit structuri
+pentru adresa url a componentelor serverului, functii de adaugare de comenzi, etc. De asemenea, 
+pentru testarea temei consider important si verificarea raspunsului de la server, astfel inainte
+de int main() {} am adaugat o variabila care pusa pe true ofera afisarea mesajului trimis de
+server in urma fiecarei comenzi. Pe langa raspunsul serverului am tratat si eu diverse erori
+precum incercarea de logare consecutiva fara un logout intre cele doua comenzi, etc.
+    In final, o tema faina si nu foarte dificil de rezolvat!
--- client.cpp
+++ client.cpp
@@ -16,6 +16,9 @@ using json = nlohmann::json;
 
 vector<string> command_options;
 
+/**
+ * Comenzile default pe care le are userul initial
+ * */
 void add_default_command_options() {
     command_options.push_back("register");
     command_options.push_back("login");
@@ -28,15 +31,26 @@ void add_default_command_options() {
     command_options.push_back("exit");
 }
 
+/**
+ * In viitor se pot adauga noi functionalitati, astfel 
+ * este folositoare o functie care sa adauge o noua comanda cand
+ * este nexesar
+ * */
 void add_command_option(string option) {
     command_options.push_back(option);
 }
 
+/**
+ * Drepturi de autentificare necesare in unele comenzi sau in incercarea spre
+ * exemplu de logare cand suntem deja logati
+ * */
 struct needed_rights {
     bool loged_in_on_server = false;
-    bool registered_to_library = false;
 };
 
+/**
+ * Creeaza prompt-ul pentru introducerea datelor utilizatorului
+ * */
 json get_user_credentials() {
     json info;
     string username, password;
@@ -49,6 +63,10 @@ json get_user_credentials() {
     return info;
 }
 
+/**
+ * Din mesajul primit de la server se extrage in mod corect cookie-ul
+ * primit
+ * */
 string extract_cookie(string initial) {
     string result = initial;
     result = strstr(initial.c_str(), "connect.sid=");
@@ -63,6 +81,10 @@ string extract_cookie(string initial) {
     return "";
 }
 
+/**
+ * Link-uri ajutoatoare pentru fiecare comanda in parte plus portul si host-ul 
+ * ip initial
+ * */
 struct server_links {
     int32_t server_port = 8080;
     char host_ip[MAX_SIZE] = "34.241.4.235";
@@ -77,6 +99,7 @@ struct server_links {
     string json_c_type = "application/json";
 };
 
+// Daca se doreste afisarea mesajelor de la server, se poate pune pe true aceasta variabila
 bool get_server_results = false;
 
 int main(int argc, char *argv[])
@@ -98,10 +121,12 @@ int main(int argc, char *argv[])
     server_links server;
     
     while (true) {
+        // Deschidem conexiunea pe serverul corespunzator, cu parametrii necesari pentru o conexiune IP
         int32_t server_socket = -1;
         server_socket = open_connection(server.host_ip, server.server_port, PF_INET, SOCK_STREAM, 0);
         ERROR(server_socket < 0, "Error, oppening connection failed");
 
+        //Afisam comenzile pe care le poate alege utilizatorul la fiecare pas
         cout << "\nEnter one of the commands : ";
         count = 0;
         for (auto option : command_options) {
@@ -118,16 +143,25 @@ int main(int argc, char *argv[])
                 break;
             }
         }
+        // Daca aceasta comanda este gresita, se repeta intrebarea de alegere
         if (!command_found) {
             cout << "Wrong command, please try again\n";
             continue;
         }
+        // In cazul in care comanda este de exit, se face shutdown serverului si se incheie 
+        // programul
         if (command == "exit") {
             int32_t check_ret = shutdown(server_socket, SHUT_RDWR);
             ERROR(check_ret < 0, "Failed to close server");
             break;       
         }
 
+        // In cazul comenzii de register, se verifica daca suntem deja logati, daca da, 
+        // se afiseaza eroarea corespunzatoare, altfel se preiau username-ul si parola 
+        // introduse de utilizator si se creeaza request-ul de tip POST
+        // la adresa serverului. In cazul unei coliziuni de username cu un cont deja existent, 
+        // se afiseaza eroarea primind mesajul de la server. In caz contrar, un nou utilizator este
+        // inregistrat 
         if (command == "register") {
             
             if (rights.loged_in_on_server) {
@@ -156,6 +190,10 @@ int main(int argc, char *argv[])
             continue;
         }
         
+        // Analog procedam la comanda de login, cu diferenta ca acum vom primi eroare in cazul in care 
+        // utilizatorul nu exista sau parola este gresita. De asemenea, la o logare corecta, vom primi
+        // si un cookie de sesiune care va fi extras si stocat intr-un string. Nu avem nevoie de mai 
+        // multe cookie-uri deoarece putem fi logati pe maxim un cont la un moment dat.
         if (command == "login") {
             
             if (rights.loged_in_on_server) {
@@ -185,8 +223,12 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // Intrarea in librarie si primirea accesului se realizeaza corect doar in cazul in care suntem 
+        // logati. Astfel, trimitem un request de tip GET cu cookie-ul corespunzator care este validat de server
+        // ca apartinand sesiunii corecte. In caz pozitiv, vom primi un token jwt de acces care va fi apoi stocat
+        // la fel ca si cookie-ul. In caz contrar, se va afisa eroarea corespunzatoare.
         if (command == "enter_library") {
-         
+            
             string server_request = compute_get_request(server.host_ip, server.enter_library_url, "", "", current_cookie);
             send_to_server(server_socket, (char *) server_request.c_str());
 
@@ -210,6 +252,12 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // Pentru a avea acces la comanda de afisare a tuturor cartilor trebuie sa
+        // fim atat logati dar si sa avem acces la biblioteca. Astfel, atat cookie-ul
+        // cat si token-ul jwt trebuie trimise catre server in acest request de tip GET.
+        // Eroarea primita va fi fie cu privire la logare, fie la accesul nepermis.
+        // La o comanda de succes, vom putea vedea id-ul si titlul fiecarei carti 
+        // prezente in biblioteca
         if (command == "get_books") {
 
             string server_request = compute_get_request(server.host_ip, server.get_books_url, "", current_token, current_cookie);
@@ -232,6 +280,10 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // In cazul comenzii de get_book, trebuie afisate informatiile particulare ale unei carti.
+        // Astfel, trebuie sa avem ca si anterior, atat o sesiune de log in dar si un token jwt. 
+        // In caz de succes, vom afisa informatiile despre cartea dorita daca id-ul acesteia 
+        // exista, in caz contrar eroare.
         if (command == "get_book") {
             
             cout << "id=";
@@ -266,6 +318,11 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // In cadrul comenzii de adaugare al unei carti, am format obiectul json corespunzator
+        // tuturor datelor necesare apoi l-am trimis serverului ca string. Serverul a verificat 
+        // calitatea datelor introduse, producand eroare in cazul in care numarul de pagini nu este 
+        // un numar intreg. Am tratat toate aceste erori si in caz de succes, am afisat mesajul 
+        // de validare.
         if (command == "add_book") {
 
             json info;
@@ -320,6 +377,9 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // Pentru comanda de stergere a unei carti, am format url-ul necesar de accesare 
+        // al cartii cu id-ul dorit, de asemenea, am tratat atat eroarea de autorizare cat si 
+        // cea in care cartea cautata nu se afla in biblioteca.
         if (command == "delete_book") {
 
             cout << "id=";
@@ -354,6 +414,10 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // Comanda ce incheie sesiunea curenta prin reactualizarea variabilei de log in pe false si 
+        // stergerea cookie-ului si a token-ului Jwt. Astfel, utilizatorul nu mai poate accesa nimic
+        // fara sa se conecteze inapoi la server. De asemenea, daca nu suntem logati, vom produce
+        // o eroare aferenta.
         if (command == "logout") {
 
             string server_request = compute_get_request(server.host_ip, server.logout_url, "", current_token, current_cookie);
